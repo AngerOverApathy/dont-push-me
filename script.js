ac902e6e45402ad1ec68af8d28dc1b1f3cc48b3d
AngerOverApathy <violasian@gmail.com>
2022-11-01 20:03:07 -0700

    initial push

---
+++ script.js
@@ -0,0 +1,10 @@
+let button = document.getElementById('btn')
+
+//curtain effect
+
+//first click "please stop pushing me"
+
+/*reset timer? how long does each effect last? 
+does not pushing the button "reset" things?*/
+
+//css property to toggle things on and off
